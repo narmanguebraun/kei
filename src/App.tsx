ec2d4ad86c8fd6d0463a35bdeef8b2ae51f1a591
narmanguebraun <narmanguebraun1@gmail.com>
2024-10-07 14:59:05 +0200

    Add prettier sort imports plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
-import "./index.css";
-import { Button } from "./components/ui/button";
+import { useState } from 'react'
+
+import './index.css'
+
+import { Button } from './components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="m-6 text-xl">Vite + React</h1>
+      <h1 className='m-6 text-xl'>Vite + React</h1>
 
-      <Button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </Button>
+      <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
