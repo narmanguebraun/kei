f829839649cc467cb9ba7933e959ac268ba92e2f
narmanguebraun <narmanguebraun1@gmail.com>
2024-10-07 12:47:38 +0200

    Add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./index.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,9 +9,9 @@ function App() {
     <>
       <h1 className="m-6 text-xl">Vite + React</h1>
 
-      <button onClick={() => setCount((count) => count + 1)}>
+      <Button onClick={() => setCount((count) => count + 1)}>
         count is {count}
-      </button>
+      </Button>
     </>
   );
 }
